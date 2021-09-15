a372b2e243ae0fe18abb654d4fb8343f3c593a80
Poinch <ponch92@hotmail.it>
2021-09-15 17:15:59 +0200

    Banner added

--- components/Layout/Banner/Banner.js
+++ components/Layout/Banner/Banner.js
@@ -1,7 +1,20 @@
+import Image from 'next/image'
+import bannerImg from '../../../public/Banner.webp'
+
 function Banner() {
   return (
-    <div>
-      I'm a Banner
+    <div className='relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]'>
+      <Image 
+        src={bannerImg}
+        layout='fill'
+        objectFit='cover'
+      />
+      <div className='absolute top-1/2 w-full text-center'>
+        <p className='text-sm sm:text-lg'>
+          Not sure where to go? Perfect.
+        </p>
+        <button className='text-purple-500 bg-white px-10 py-4 rounded-full shadow-md font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150'>I'm Flexible</button>
+      </div>
     </div>
   )
 }
